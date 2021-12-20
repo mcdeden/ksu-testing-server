d0007e33918b11bd0e6a72a57cbab642997abf74
Deden Ferdiansyah <mcdeden@gmail.com>
2021-12-20 22:43:50 +0700

    project

---
+++ src/test/java/com/example/ksutestingserver/KsuTestingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ksutestingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsuTestingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
